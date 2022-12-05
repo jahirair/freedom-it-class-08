daa9ccab5e48c6a55694b2190ea1971719b332ff
jahirair <jahirair@gmail.com>
2022-12-05 06:32:33 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*Array*/
+
+var color = ["Black","White","Blue","Tomato","Green","Red","Yellow"]
+document.getElementById("cml").innerHTML= color[0];
+
+var car = ["BMW","Volvo","Boom & Cool"]
+document.getElementById("bd").innerHTML=car[1];
